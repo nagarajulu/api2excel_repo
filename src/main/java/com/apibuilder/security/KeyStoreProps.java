7b860bbc1889c698b76825cbe74e06445e3c8e94
Nagarajulu Aerakoni <nagarajulu.a@gmail.com>
2019-09-22 22:14:20 -0700

    #4 updated keystore path

--- src/main/java/com/apibuilder/security/KeyStoreProps.java
+++ src/main/java/com/apibuilder/security/KeyStoreProps.java
@@ -3,7 +3,7 @@ package com.apibuilder.security;
 public class KeyStoreProps {
 
     public static final String keystorePasswordPath="/api2excel/keyStorePassword";
-    public static final String keystorePath="C:/Users/nagaraj/api2excel-csr/api2excel.jks";
+    public static final String keystorePath="/home/ubuntu/Api2ExcelApp/KeyStore/api2excel.jks";
     public static final String keystoreType="JKS";
     public static final String keyAlias="tq-2ce07367-22ee-4d71-8a10-02c6ba45ac5c";
 }
